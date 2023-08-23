d2d507ed354b932cf74268d448e3d96c582d045b
conteudo-orbeai <107652253+conteudo-orbeai@users.noreply.github.com>
2023-08-22 22:58:05 -0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'IA Generativa na Prática'
+copyright = '2023, Orbe.ai'
+author = 'Orbe.ai'
 
 release = '0.1'
 version = '0.1.0'
